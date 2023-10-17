82186ed8ddecdb6ce94a9a7d5aaa2a557c8b5953
Duy Hoang (duyhoan) <duyhoan@cisco.com>
2023-10-17 14:09:13 -0700

    update to take and merge all config YAML files

--- plugins/filter/filters.py
+++ plugins/filter/filters.py
@@ -50,10 +50,31 @@ def extractor(data, *paths):
     # Begin extraction from the top-level data
     return extract_values(data, paths, "")
 
+
+def deep_merge_dicts(dict1, dict2):
+    """
+    Recursively merges dict2 into dict1.
+    """
+    for key, value in dict2.items():
+        if key in dict1:
+            # If both are dictionaries, merge them
+            if isinstance(dict1[key], dict) and isinstance(value, dict):
+                deep_merge_dicts(dict1[key], value)
+            # If both are lists, concatenate them
+            elif isinstance(dict1[key], list) and isinstance(value, list):
+                dict1[key].extend(value)
+            else:
+                dict1[key] = value
+        else:
+            dict1[key] = value
+    return dict1
+
+
 class FilterModule(object):
     ''' Ansible core jinja2 filters '''
 
     def filters(self):
         return {
             'extractor': extractor,
+            'deep_merge_dicts': deep_merge_dicts
         }
