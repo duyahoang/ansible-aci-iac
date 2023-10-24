0c28e79d500a65c98ae6c0f593cf7888f1e2277a
Duy Hoang (duyhoan) <duyhoan@cisco.com>
2023-10-24 07:06:42 -0700

    Enhance filters.py

--- plugins/filter/filters.py
+++ plugins/filter/filters.py
@@ -1,20 +1,19 @@
 # flake8: noqa E501
+from typing import List, Dict, Any, Union, Optional
 from ansible.errors import AnsibleError
-import yaml
 import json
 
-def extractor(data, defaults, path):
+def extractor(data: Dict[str, Any], defaults: Dict[str, Any], path: str) -> List[Dict[str, Any]]:
     """
-    Traverse the data based on the given path and return scalar values along \
-          the path.
-
+    Traverse the data based on the given path and return scalar values along the path.
+    
     Args:
-    - data (dict): The data to traverse.
+    - data (Dict[str, Any]): The data to traverse.
+    - defaults (Dict[str, Any]): Default values to be merged if certain keys are missing.
     - path (str): Path to traverse.
-
+    
     Returns:
-    - List[Dict]: A list of dictionaries containing scalar values along \
-          the path.
+    - List[Dict[str, Any]]: A list of dictionaries containing scalar values along the path.
     """
 
     def extract_values(data, paths, prefix):
@@ -75,170 +74,351 @@ def extractor(data, defaults, path):
         # If path not found, return empty
         return []
 
+    if not isinstance(data, dict) or not isinstance(path, str):
+        raise AnsibleError("Invalid input data or path provided to extractor.")
+
     # Begin extraction from the top-level data
     paths = path.split('/')
-    outter_results = extract_values(data, paths, "")
-    defaults = extract_values(defaults, paths, "")
-    final_result = []
-    for entry in outter_results:
-        if defaults:
-            for default_key, default_value in defaults[0].items():
-                if default_key not in entry:
-                    entry[default_key] = default_value
-        final_result.append(entry)
+    
+    if not paths:
+        raise AnsibleError(f"Invalid path provided: {path}.")
+    
+    try:
+        outter_results = extract_values(data, paths, "")
+    except Exception as e:
+        raise AnsibleError(f"Error in extractor filter while extract data from {path}: {str(e)}")
+    
+    if not isinstance(defaults, dict):
+        raise AnsibleError("Invalid defaults provided to extractor.")
+    try:
+        defaults = extract_values(defaults, paths, "")
+    except Exception as e:
+        raise AnsibleError(f"Error in extractor filter while extract defaults from {path}: {str(e)}")
+    
+    try:
+        final_result = []
+        for entry in outter_results:
+            if defaults:
+                for default_key, default_value in defaults[0].items():
+                    if default_key not in entry:
+                        entry[default_key] = default_value
+            final_result.append(entry)
+    except Exception as e:
+        raise AnsibleError(f"Error in extractor filter while merge data and defaults: {str(e)}")
+    
     return final_result
 
 
-def deep_merge_dicts(dict1, dict2):
+def deep_merge_dicts(dict1: Dict[str, Any], dict2: Dict[str, Any]) -> Dict[str, Any]:
     """
     Recursively merges dict2 into dict1.
+    
+    Args:
+    - dict1 (Dict[str, Any]): The primary dictionary that will be merged into.
+    - dict2 (Dict[str, Any]): The dictionary that will be merged from.
+    
+    Returns:
+    - Dict[str, Any]: The merged dictionary.
+    
+    Raises:
+    - AnsibleError: If either of the input is not a dictionary or any other error occurs.
     """
-    for key, value in dict2.items():
-        if key in dict1:
-            # If both are dictionaries, merge them
-            if isinstance(dict1[key], dict) and isinstance(value, dict):
-                deep_merge_dicts(dict1[key], value)
-            # If both are lists, concatenate them
-            elif isinstance(dict1[key], list) and isinstance(value, list):
-                dict1[key].extend(value)
+    
+    try:
+        if not isinstance(dict1, dict) or not isinstance(dict2, dict):
+            raise AnsibleError("Both inputs to deep_merge_dicts should be dictionaries.")
+    
+        for key, value in dict2.items():
+            if key in dict1:
+                if isinstance(dict1[key], dict) and isinstance(value, dict):
+                    deep_merge_dicts(dict1[key], value)
+                elif isinstance(dict1[key], list) and isinstance(value, list):
+                    dict1[key].extend(value)
+                else:
+                    dict1[key] = value
             else:
                 dict1[key] = value
-        else:
-            dict1[key] = value
-    return dict1
+
+        return dict1
+
+    except Exception as e:
+        raise AnsibleError(f"Error in deep_merge_dicts filter: {str(e)}")
 
 
-def bool_converter(boolean):
+def bool_converter(value: Union[bool, Any]) -> str:
     """
-    Convert boolean true/false to enabled/disabled.
-    """
-    if isinstance(boolean, bool):
-        if boolean is True:
-            return "enabled"
-        return "disabled"
-    return boolean
-
-
-def list_assembler(item, options, names):
-    results = []
-    for index, data in enumerate(names):
-        if data in item:
-            if item[data]:
-                results.append(options[index])
-    return results
-
-
-def ip_cidr_extractor(data, part):
-    ip_address, _, cidr = data.partition('/')
+    Convert boolean true/false to 'enabled'/'disabled'.
     
-    if part == "ip":
-        return ip_address
-    elif part == "cidr":
-        return cidr
-    else:
-        raise ValueError("The 'part' argument must be either 'ip' or 'cidr'.")
+    Args:
+    - value (Union[bool, Any]): The value to convert. Expected to be a boolean, 
+                                but other types are returned as-is.
+    
+    Returns:
+    - str: 'enabled' if value is True, 'disabled' if value is False, 
+           otherwise returns the value itself.
+    
+    Raises:
+    - AnsibleError: If there's an error during the conversion process.
+    """
+    
+    try:
+        if isinstance(value, bool):
+            return "enabled" if value else "disabled"
+        return value
+
+    except Exception as e:
+        raise AnsibleError(f"Error in bool_converter filter: {str(e)}")
 
 
-def static_ports_assembler(data):
-    result = []
-    grouped_data = {}
-    prefix = "apic_tenants_application_profiles_endpoint_groups_static_ports"
+def list_assembler(item: Dict[str, Any], options: List[str], names: List[str]) -> List[str]:
+    """
+    Assemble a list based on the presence of keys in the input item.
 
-    for entry in data:        
-        key = (entry['apic_tenants_name'], entry['apic_tenants_application_profiles_name'], entry['apic_tenants_application_profiles_endpoint_groups_name'])
-        if key not in grouped_data:
-            grouped_data[key] = {
+    Args:
+    - item (Dict[str, Any]): The dictionary containing key-value pairs.
+    - options (List[str]): A list of strings representing possible options.
+    - names (List[str]): A list of keys that we need to check in the item.
+
+    Returns:
+    - List[str]: A list containing selected options based on the presence of names in the item.
+    
+    Raises:
+    - AnsibleError: If there's an error during the list assembly process.
+    """
+    
+    try:
+        results = []
+        for index, data in enumerate(names):
+            if data in item and item[data]:
+                results.append(options[index])
+        return results
+
+    except Exception as e:
+        raise AnsibleError(f"Error in list_assembler filter: {str(e)}")
+
+
+def ip_cidr_extractor(data: str, part: str) -> Optional[str]:
+    """
+    Extract IP address or CIDR from a given IP/CIDR string.
+
+    Args:
+    - data (str): The IP/CIDR string.
+    - part (str): The part to extract, either 'ip' or 'cidr'.
+
+    Returns:
+    - Optional[str]: Extracted IP or CIDR part. Returns None if the part isn't found.
+    
+    Raises:
+    - AnsibleError: If there's an error during the extraction or if an invalid 'part' argument is provided.
+    """
+    
+    try:
+        ip_address, _, cidr = data.partition('/')
+        
+        if part == "ip":
+            return ip_address
+        elif part == "cidr":
+            return cidr
+        else:
+            raise ValueError("The 'part' argument must be either 'ip' or 'cidr'.")
+
+    except Exception as e:
+        raise AnsibleError(f"Error in ip_cidr_extractor filter: {str(e)}")
+
+
+def static_ports_assembler(data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """
+    Assemble static port configurations based on the provided data.
+
+    Args:
+    - data (List[Dict[str, Any]]): The list of dictionaries containing static port configurations.
+
+    Returns:
+    - List[Dict[str, Any]]: A list of dictionaries with assembled static port configurations.
+    
+    Raises:
+    - AnsibleError: If there's an error during the assembly process.
+    """
+    def get_key(entry):
+        return (
+            entry['apic_tenants_name'], 
+            entry['apic_tenants_application_profiles_name'], 
+            entry['apic_tenants_application_profiles_endpoint_groups_name']
+        )
+
+    try:
+        grouped_data = {}
+        prefix = "apic_tenants_application_profiles_endpoint_groups_static_ports"
+
+        for entry in data:        
+            key = get_key(entry)
+
+            grouped_data.setdefault(key, {
                 "tenant": entry['apic_tenants_name'],
                 "ap": entry['apic_tenants_application_profiles_name'],
                 "epg": entry['apic_tenants_application_profiles_endpoint_groups_name'],
                 "interface_configs": []
+            })
+
+            leafs = [entry[f'{prefix}_node_id']]
+            if f'{prefix}_node2_id' in entry:
+                leafs.append(entry[f'{prefix}_node2_id'])
+            
+            pod_id = entry[f'{prefix}_pod_id']
+            extpaths = []
+            interface = entry.get(f'{prefix}_channel', f'{entry[prefix + "_module"]}/{entry[prefix + "_port"]}')
+
+            if f'{prefix}_node_id' in entry and f'{prefix}_channel' not in entry:
+                
+                if f'{prefix}_sub_port' in entry:
+                    interface += f'/{entry[prefix + "_sub_port"]}'
+                    interface_type = 'switch_port'
+                elif f'{prefix}_fex_id' in entry:
+                    extpaths = [str(entry[f'{prefix}_fex_id'])]
+                    interface_type = 'fex'
+                else:
+                    interface_type = 'switch_port'
+            else:
+                interface_type = 'vpc' if f'{prefix}_node2_id' in entry else 'port_channel'
+
+            interface_config = {
+                "encap_id": entry[f'{prefix}_vlan'],
+                "interface": interface,
+                "leafs": leafs,
+                "pod_id": pod_id,
+                "interface_mode": entry[f'{prefix}_mode'],
+                "deploy_immediacy": entry[f'{prefix}_deployment_immediacy'],
+                "interface_type" : interface_type,
             }
 
-        if f'{prefix}_node2_id' in entry:
-            leafs = [entry[f'{prefix}_node_id'], entry[f'{prefix}_node2_id']]
-        else:
-            leafs = entry[f'{prefix}_node_id']
+            if extpaths:
+                interface_config["extpaths"] = extpaths
+            if f'{prefix}_description' in entry:
+                interface_config["description"] = entry[f'{prefix}_description'] 
 
-        extpaths = []
-        if f'{prefix}_node_id' in entry and f'{prefix}_channel' not in entry:
-            pod_id = entry[f'{prefix}_pod_id']
-            if f'{prefix}_sub_port' in entry:
-                interface = f'{entry[prefix + "_module"]}/{entry[prefix + "_port"]}/{entry[prefix + "_sub_port"]}'
-                interface_type = 'switch_port'
-            elif f'{prefix}_fex_id' in entry:
-                extpaths = [str(entry[f'{prefix}_fex_id'])]
-                interface = f'{entry[prefix + "_module"]}/{entry[prefix + "_port"]}'
-                interface_type = 'fex'
-            else:
-                interface = f'{entry[prefix + "_module"]}/{entry[prefix + "_port"]}'
-                interface_type = 'switch_port'
-        else:
-            interface = entry[f'{prefix}_channel']
-            if f'{prefix}_node2_id' in entry:
-                interface_type = 'vpc'
-            else:
-                interface_type = 'port_channel'
+            grouped_data[key]['interface_configs'].append(interface_config)
 
-        interface_config = {
-            "encap_id": entry[f'{prefix}_vlan'],
-            "interface": interface,
-            "leafs": leafs,
-            "pod_id": pod_id,
-            "interface_mode": entry[f'{prefix}_mode'],
-            "deploy_immediacy": entry[f'{prefix}_deployment_immediacy'],
-            "interface_type" : interface_type,
-        }
-        if extpaths:
-            interface_config["extpaths"] = extpaths
-        if f'{prefix}_description' in entry:
-            interface_config["description"] = entry[f'{prefix}_description'] 
+        return list(grouped_data.values())
 
-        grouped_data[key]['interface_configs'].append(interface_config)
-
-    result = list(grouped_data.values())
-    return result
+    except Exception as e:
+        raise AnsibleError(f"Error in static_ports_assembler filter: {str(e)}")
 
 
-def list_of_dict_diff(list1, list2):
-    set1 = {json.dumps(d, sort_keys=True) for d in list1}
-    set2 = {json.dumps(d, sort_keys=True) for d in list2}
+def list_of_dict_diff(list1: list, list2: list) -> list:
+    """
+    Determine the difference between two lists of dictionaries.
     
-    diff = set2 - set1
-
-    return [json.loads(s) for s in diff]
-
-
-def present_extracted_data_assembler(previous_dict, current_dict):
-    results = {}
-    for key, value in current_dict.items():
-        if key in previous_dict:
-            dict_diff = list_of_dict_diff(previous_dict[key], current_dict[key])
-            if dict_diff:
-                results[key]= dict_diff
-        else:
-            results[key] = value
-    return results
-
-
-def absent_extracted_data_assembler(previous_dict, current_dict):
-    results = {}
-    for key, value in previous_dict.items():
-        if key in current_dict:
-            dict_diff = list_of_dict_diff(current_dict[key],previous_dict[key])
-            if dict_diff:
-                results[key]= dict_diff
-        else:
-            results[key] = value
-    return results
+    Args:
+        list1 (list): The first list of dictionaries.
+        list2 (list): The second list of dictionaries.
+        
+    Returns:
+        list: A list of dictionaries that are in list2 but not in list1.
+        
+    Raises:
+        AnsibleError: If an error occurs during the process.
+    """
+    try:
+        set1 = {json.dumps(d, sort_keys=True) for d in list1}
+        set2 = {json.dumps(d, sort_keys=True) for d in list2}
+        
+        diff = set2 - set1
+        return [json.loads(s) for s in diff]
+    except Exception as e:
+        raise AnsibleError(f"Error in 'list_of_dict_diff' func: {str(e)}")
 
 
-def find_value(extracted_data, path, identified_key, identified_value, key_to_find):
-    if path in extracted_data:
-       for item in extracted_data[path]:
-           if identified_key in item and item[identified_key] == identified_value:
-               if key_to_find in item:
-                   return item[key_to_find]
-    return None
+def present_extracted_data_assembler(previous_dict: dict, current_dict: dict) -> dict:
+    """
+    Assemble the present extracted data by determining the difference between
+    the current dictionary and the previous dictionary.
+    
+    Args:
+        previous_dict (dict): The previous dictionary.
+        current_dict (dict): The current dictionary.
+        
+    Returns:
+        dict: A dictionary with keys from the current dictionary where the values
+              are different from those in the previous dictionary.
+        
+    Raises:
+        AnsibleError: If an error occurs during the process.
+    """
+    try:
+        results = {}
+        for key, value in current_dict.items():
+            # If the key exists in both dictionaries, find the difference
+            if key in previous_dict:
+                dict_diff = list_of_dict_diff(previous_dict[key], current_dict[key])
+                if dict_diff:
+                    results[key] = dict_diff
+            # If the key only exists in the current dictionary, add it to the results
+            else:
+                results[key] = value
+        return results
+    except Exception as e:
+        raise AnsibleError(f"Error in 'present_extracted_data_assembler' filter: {str(e)}")
+
+
+def absent_extracted_data_assembler(previous_dict: dict, current_dict: dict) -> dict:
+    """
+    Assemble the absent extracted data by determining the difference between
+    the previous dictionary and the current dictionary.
+    
+    Args:
+        previous_dict (dict): The previous dictionary.
+        current_dict (dict): The current dictionary.
+        
+    Returns:
+        dict: A dictionary with keys from the previous dictionary where the values
+              are different from those in the current dictionary.
+        
+    Raises:
+        AnsibleError: If an error occurs during the process.
+    """
+    try:
+        results = {}
+        for key, value in previous_dict.items():
+            # If the key exists in both dictionaries, find the difference
+            if key in current_dict:
+                dict_diff = list_of_dict_diff(current_dict[key], previous_dict[key])
+                if dict_diff:
+                    results[key] = dict_diff
+            # If the key only exists in the previous dictionary, add it to the results
+            else:
+                results[key] = value
+        return results
+    except Exception as e:
+        raise AnsibleError(f"Error in 'absent_extracted_data_assembler' filter: {str(e)}")
+
+
+def find_value(extracted_data: dict, path: str, identified_key: str, identified_value: str, key_to_find: str) -> str:
+    """
+    Search within the extracted data to find a specific value based on the provided path, identified key, and value.
+    
+    Args:
+        extracted_data (dict): The data to search within.
+        path (str): The path indicating where to look within the data.
+        identified_key (str): The key to identify the desired data.
+        identified_value (str): The value to identify the desired data.
+        key_to_find (str): The key whose value needs to be found.
+        
+    Returns:
+        str: The value associated with the key_to_find if found, otherwise None.
+        
+    Raises:
+        AnsibleError: If an error occurs during the search process.
+    """
+    try:
+        # Check if the path exists in the extracted_data
+        if path in extracted_data:
+            for item in extracted_data[path]:
+                # Match based on the identified_key and identified_value
+                if identified_key in item and item[identified_key] == identified_value:
+                    return item.get(key_to_find)
+        return None
+    except Exception as e:
+        raise AnsibleError(f"Error in 'find_value' filter: {str(e)}")
 
 
 class FilterModule(object):
